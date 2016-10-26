ad4c59912103fd0598b45c65e6e0e4ffd076eced
marcimmediato <marc.immediato@gmail.com>
2016-10-26 17:32:47 -0400

    Create models

---
+++ db/migrate/20161026212848_create_collections.rb
@@ -0,0 +1,10 @@
+class CreateCollections < ActiveRecord::Migration[5.0]
+  def change
+    create_table :collections do |t|
+      t.string :type
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end

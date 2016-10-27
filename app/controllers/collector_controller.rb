311c035064794a0c662df34dc7bd124cc87e3260
lshapz <laurashapiro01@gmail.com>
2016-10-27 10:48:49 -0400

    collector

--- app/controllers/collector_controller.rb
+++ app/controllers/collector_controller.rb
@@ -1,6 +1,9 @@
 class CollectorController < ApplicationController
   def new
   end
+  def index
+    @collectors = Collector.all
+  end
 
   def create
   end
@@ -13,5 +16,7 @@ class CollectorController < ApplicationController
   end
 
   def show
+    @collector = Collector.find(params[:id])
+    #this might need to be chaned to user_id? or nah because has one?
   end
 end

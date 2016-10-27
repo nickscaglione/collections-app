9080869692a12a19cc107f98d8db304700026ca9
lshapz <laurashapiro01@gmail.com>
2016-10-27 10:04:26 -0400

    moved to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161027135635) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.string   "name"
     t.integer  "count"

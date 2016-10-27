a5ace7a2da9e996cd390703c8c27cae32e4aa32e
lshapz <laurashapiro01@gmail.com>
2016-10-27 09:52:38 -0400

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'collector/new'
 
-  get 'collector/show'
-
-  get 'collector/edit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :cards
   resources :collections
+  resources :collector 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'

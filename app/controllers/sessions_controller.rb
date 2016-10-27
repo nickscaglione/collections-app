f2aeced586ab97adfa0257e42234d5e98d949b19
marcimmediato <marc.immediato@gmail.com>
2016-10-27 14:07:45 -0400

    added auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class SessionsController < ApplicationController
   def new
     # adding a new user to be used in the sign-in form_for; the form will need
@@ -6,9 +8,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(user_name: params[:session][:user_name])
+    return head(:forbidden) unless @user.authenticate(params[:session][:password])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to users_path
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,10 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end

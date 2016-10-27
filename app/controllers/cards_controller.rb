b5e0896475e9ce6be890fbf3856d41a1707e4946
lshapz <laurashapiro01@gmail.com>
2016-10-27 16:02:49 -0400

    fixed bug with cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -12,35 +12,41 @@ class CardsController < ApplicationController
     if params[:card][:collection] == "other"
       @collection = Collection.create(owner: @user.owner, category: params[:collection])
     else 
+     #  byebug
       @collection = Collection.find_by(category: params[:card][:collection])
+      
     end  
-      @card = Card.create(name: params[:card][:name], count: params[:card][:count], collection: @collection)
-      # byebug
+    @card = Card.create(name: params[:card][:name], count: params[:card][:count], collection_id: @collection.id)
+    byebug
     redirect_to card_path(@card)
   end
 
   def edit
     @card = Card.find(params[:id])
-        @user = User.find(current_user)
+    # byebug
+    @user = User.find(current_user)
 
   end
 
   def update
     @card = Card.find(params[:id])
-
+    @user = User.find(current_user)
+    byebug
    if params[:card][:collection] == "other"
-      @collection = Collection.create(owner: @user.owner, category: params[:collection]).id
+      @collection = Collection.create(owner: @user.owner, category: params[:collection])
     else 
-      @collection = Collection.find_by(category: params[:card][:collection]).id
+      byebug 
+      @collection = Collection.find_by(category: params[:card][:collection])
+      #
     end  
-    params[:card][:collection_id] = @collection
+    params[:card][:collection_id] = @collection.id
 #    byebug
-
+    # params[:card][:user_name]
     @card.update(params[:card].permit(:collection_id, :name, :count))
     redirect_to card_path(@card)
   end
 
   def show
-    @card = Card.find(params[:id])
+    @card = Card.find(params[:id]) 
   end
 end

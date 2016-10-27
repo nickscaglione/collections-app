b237cdb6eb8909497c077a0e2e2eee372b61cc8c
lshapz <laurashapiro01@gmail.com>
2016-10-27 10:09:12 -0400

    login helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end 
+  end
+
+private
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end     
+
+
+
 end

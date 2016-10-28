ddc9fc2a267a3c9aec0886cb04603e557e9c5712
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-10-28 16:18:13 -0400

    edit decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -18,20 +18,30 @@ class DecksController < ApplicationController
     params[:card].each do |card_id|
      count = params[:card].count(card_id)
      count_hash[card_id] = count
-    end 
+    end
     count_hash.each do |card_id, card_count|
       CardDeck.create(deck_id: @deck.id, card_id: card_id, card_count: card_count)
-    end 
+    end
       # byebug
 
     redirect_to see_decks_path(@deck.owner)
   end
 
   def edit
+    @deck = Deck.find(params[:id])
+    # find cards not included in the deck but are included in the deck's parent brand
+    @unused_cards = @deck.brand.cards.select { |card| !@deck.cards.pluck(:name).include?(card.name) }
   end
 
   def update
+    @deck = Deck.find(params[:id])
+    CardDeck.destroy_all(deck_id: @deck.id)
+    count_hash = {}
+    params[:card].each do |card_id, card_count|
+      CardDeck.create(deck_id: @deck.id, card_id: card_id, card_count: card_count)
+    end
 
+    redirect_to @deck
   end
 
   def show
@@ -57,7 +67,7 @@ class DecksController < ApplicationController
         if deck.owner == @owner
           @my_decks << deck
         end
-      end 
-    
+      end
+
   end
 end

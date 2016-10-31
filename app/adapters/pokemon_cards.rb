5e0ab07b7483613c1db00b276125e0975dbc58d3
lshapz <laurashapiro01@gmail.com>
2016-10-31 11:17:14 -0400

    pokemon api also finds year

--- app/adapters/pokemon_cards.rb
+++ app/adapters/pokemon_cards.rb
@@ -3,16 +3,25 @@ require 'byebug'
 
 class PokemonCards
 
-  def self.find_card(card_name)
+  def self.find_card(card_name, year_min = 1996, year_max = 2016)
     #cards = Pokemon::Card.where(set_code: 'base1').where(set: 'Base').where(name: card_name).where(series: 'Base').all
     
     # cards.each do |x| 
     cards = Pokemon::Card.where(name: card_name).all#   puts x.name
     
     cards_array = cards.each_with_object([]) do |card, card_array|
-      card_array << [card.name, card.image_url]
+      card_array << [card.name, card.image_url, PokemonCards.find_set_year(card.set)]
     end 
 
+    cards_array.reject {|card| card.last < year_min || card.last > year_max}
+
+  end 
+
+
+
+  def self.find_set_year(set_name)
+    set = Pokemon::Set.where(name: set_name).all.first
+    set.release_date.split("/").last.to_i
   end 
 
 end 

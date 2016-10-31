54cbbccd29056ea86927cedbdc5c5313dc94be23
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-10-31 14:52:09 -0400

    must choose a brand when making a new card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,49 +13,53 @@ class CardsController < ApplicationController
   end
 
   def choose
-    # byebug
     @card = Card.new
     @owner = Owner.find_by(user_id: current_user.id)
-    @brand = Brand.find_by(category: params[:brand], owner_id: @owner.id) 
-    if @brand.api_name == "Pokemon"
-     @card_options =   PokemonCards.find_card(params[:search_term], params[:date][:year_min].to_i, params[:date][:year_max].to_i)
-    elsif @brand.api_name == "Magic The Gathering"
-      @card_options =  MagicTheGathering.find_card(params[:search_term], params[:date][:year_min].to_i, params[:date][:year_max].to_i )
-    end 
-
-  end 
+    #make sure brand is selected
+    if !params[:brand]
+      flash[:notice] = "You must select a collection!"
+      render :new
+    else
+      @brand = Brand.find_by(category: params[:brand], owner_id: @owner.id)
+      if @brand.api_name == "Pokemon"
+       @card_options = PokemonCards.find_card(params[:search_term], params[:date][:year_min].to_i, params[:date][:year_max].to_i)
+      elsif @brand.api_name == "Magic The Gathering"
+        @card_options = MagicTheGathering.find_card(params[:search_term], params[:date][:year_min].to_i, params[:date][:year_max].to_i )
+      end
+    end
+  end
 
   def create
     # byebug
     @card = Card.new
     @owner = Owner.find_by(user_id: current_user.id)
-    @card = Card.new(name: params[:name], image_url: params[:card][:image_url], count: params[:card][:count], brand_id: params[:brand_id]) 
+    @card = Card.new(name: params[:name], image_url: params[:card][:image_url], count: params[:card][:count], brand_id: params[:brand_id])
     if !@card.save
-      flash[:notice] = "You need at least 1 of the card to add it!" 
-      @card_options = [[@card.name, @card.image_url]] 
+      flash[:notice] = "You need at least 1 of the card to add it!"
+      @card_options = [[@card.name, @card.image_url]]
       # byebug
         @card = Card.new
         @owner = Owner.find_by(user_id: current_user.id)
-        @brand = Brand.find(params[:brand_id]) 
+        @brand = Brand.find(params[:brand_id])
       render :choose
     else
         redirect_to card_path(@card)
-    end    
+    end
   end
 
   def index
     all = Card.all
     mine = all.select {|card| card.brand.owner.user_id == current_user.id}
     @cards = mine.sort_by {|card| card.brand}
-    @poke = [] 
+    @poke = []
     @magic = []
-    @cards.each do |card| 
+    @cards.each do |card|
       # byebug
       if card.brand.category == "Pokemon"
       @poke << [card.name, card.count]
     elsif card.brand
       @magic << [card.name, card.count]
-    end 
+    end
   end
 
   end
@@ -88,7 +92,7 @@ class CardsController < ApplicationController
   def show
     if Card.find(params[:id]) == nil
       redirect_to new_card_path
-    else 
+    else
       @card = Card.find(params[:id])
     end
   end

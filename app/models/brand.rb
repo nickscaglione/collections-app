730b3e8167b7cb94c833317af57b681cdd0f6fed
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-10-30 21:59:56 -0400

    brand knows its card with most deck appearances and rspec for this

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -12,17 +12,29 @@ class Brand < ApplicationRecord
       if current_user.owner.brands.pluck(:category).include?(self.category)
         errors.add(self.category, "category already in use!")
       end
-    end 
+    end
   end
 
   def decks
     decks = []
     self.cards.each do |card|
       card.decks.each do |deck|
-        decks << deck 
+        decks << deck
       end
     end
     decks
+  end
+
+  # return the card in the brand that is featured in the most decks (number of times used in a deck not considered)
+  def most_used_card
+    sql = <<-SQL
+      SELECT cards.id, MAX(cards.name) as name, MAX(cards.created_at) AS created_at, MAX(cards.updated_at) as updated_at, COUNT(cards.id) AS counter
+      FROM cards
+      LEFT JOIN card_decks ON cards.id = card_decks.card_id
+      WHERE cards.brand_id = #{self.id}
+      GROUP BY cards.id ORDER BY counter DESC LIMIT 1
+    SQL
+    Card.find_by_sql(sql).first
   end
 
 end

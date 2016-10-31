64e79893c59458fe4ce515b8fd84b90b663f97b1
lshapz <laurashapiro01@gmail.com>
2016-10-31 11:36:23 -0400

    added API name to brands

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161028131849) do
   create_table "brands", force: :cascade do |t|
     t.string   "category"
     t.integer  "owner_id"
+    t.string   "api_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

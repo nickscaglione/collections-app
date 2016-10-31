66aab6040ce10644822d89edc4a3ed2ea98854c0
lshapz <laurashapiro01@gmail.com>
2016-10-31 11:07:14 -0400

    find_by_year

--- app/adapters/pokemon_cards.rb
+++ app/adapters/pokemon_cards.rb
@@ -4,14 +4,15 @@ require 'byebug'
 class PokemonCards
 
   def self.find_card(card_name)
-    cards = Pokemon::Card.where(set_code: 'base1').where(set: 'Base').where(name: card_name).where(series: 'Base').all
-    cards.each do |x| 
-      puts x.name
-      puts x.image_url
-      puts x.id
-      puts x.national_pokedex_number
-    end
+    #cards = Pokemon::Card.where(set_code: 'base1').where(set: 'Base').where(name: card_name).where(series: 'Base').all
     
+    # cards.each do |x| 
+    cards = Pokemon::Card.where(name: card_name).all#   puts x.name
+    
+    cards_array = cards.each_with_object([]) do |card, card_array|
+      card_array << [card.name, card.image_url]
+    end 
+
   end 
 
 end 
